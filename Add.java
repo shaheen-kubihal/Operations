d75354995fdca6096d4b8b33f79ae6df8dea1f63
shaheen-kubihal <shaheenkubihal@gmail.com>
2025-05-06 02:59:41 -0700

    Save

--- Add.java
+++ Add.java
@@ -1,9 +1,10 @@
-public class Add {
-  
-        public static void main(String[] args) {
-            int a=20; int b=5;
-            int sum=a+b;
-            System.out.println("Addition : "+sum);
-            
-}
-}
+public class Add{
+    public static void main(String[] args) {
+        int a=20; int b=5;
+        System.out.println("Addition : "+(a+b));
+    
+
+      
+    
+    }
+    }
