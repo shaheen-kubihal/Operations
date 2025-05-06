1a8b87ae916b5c60d0bfcd36814145318ecb6010
shaheen-kubihal <shaheenkubihal@gmail.com>
2025-05-06 02:12:32 -0700

    Save the employee details

---
+++ Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+    String name;
+    int id;
+    double salary;
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+    public void displayinfo() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("XYZ", 101, 50000.0);
+        emp1.displayinfo();
+    }
+}
